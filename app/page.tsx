87cc944b4c58117fb27b94931196c9bd2e9a92ac
John <yangtianzhuo970425@gmail.com>
2023-12-25 00:32:30 +1300

    Intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Intro from "@/components/intro";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="flex flex-col items-center px-4">
+      <Intro />
     </main>
   )
 }
